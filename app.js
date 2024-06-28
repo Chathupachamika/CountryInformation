81171801acaff6db13c5df9f60818530ddad9894
Chathupachamika <Chathupachamika765@gmail.com>
2024-06-28 22:41:00 +0530

    first commit

--- app.js
+++
@@ -1,9 +0,0 @@
-function calc() {
-    let number1 =  Number(document.getElementById("number01").value);
-    let number2 =  Number(document.getElementById("number02").value);
-    let op = document.getElementById("op").value;
-    let lblOutput = document.getElementById("output");
-
-    
-console.log(number1 + number2);
-}
