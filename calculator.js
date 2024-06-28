69b4bc520242e78be3f2800b5e6f8f687872f689
Chathupachamika <Chathupachamika765@gmail.com>
2024-06-28 22:11:42 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,26 @@
+//-------------- CALCULATOR CODES --------------------------------
+function calc() {
+    let number1 = Number(document.getElementById("number01").value);
+    let number2 = Number(document.getElementById("number02").value);
+    
+    let op = document.getElementById("op").value;
+    let lblOutput = document.getElementById("output");
+    
+    switch(op){
+        case "+":
+            lblOutput.innerHTML = number1 + number2;
+            break;
+        case "-":
+            lblOutput.innerHTML = number1 - number2;
+            break;
+        case "*":
+            lblOutput.innerHTML = number1 * number2;
+            break;
+        case "/":
+            lblOutput.innerHTML = number1 / number2;
+            break;
+        default:
+            lblOutput.innerHTML = "Invalid operator";
+    }
+}
+//---------------------------------------------------------------------
